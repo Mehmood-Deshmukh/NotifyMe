25a2fc63119b9f2489ce6c37e3c355a56ddd1dea
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-10-17 00:27:33 +0530

    fix: node-cluster

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,9 @@ dotenv.config();
 
 const numCPUs = os.cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-} else {
+function startServer() {
   const app = express();
-
   app.use(express.json());
-
   app.use(cors());
 
   setupWebPush();
@@ -39,9 +28,26 @@ if (cluster.isMaster) {
   app.get('/', (req, res) => {
     res.send('Welcome to the Notification Manager App with Supabase and Drizzle ORM');
   });
-  
+
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Worker ${process.pid} running on port ${PORT}`);
   });
+
+  return app;
 }
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  startServer();
+}
+
+export default startServer;
